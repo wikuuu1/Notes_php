53f3d85c71b7486613a83e83ce29bf36755d0e9d
Wiktoria Rajba <w.rajba@befard.pl>
2022-04-21 15:00:13 +0200

    started notes project

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+include "src/utils/debug.php";
+
+$test = 'test';
+dump($test);
---
+++ src/utils/debug.php
@@ -0,0 +1,9 @@
+<?php
+
+function dump($data)
+{
+    echo '<div><pre>';
+    print_r($data);
+    echo '</pre></div';
+
+}
